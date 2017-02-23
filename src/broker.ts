5ee2eab351a4d8ea6eacd45f728785f93cf894d1
Rodrigo P. Fraga <rodrigo.p.fraga@gmail.com>
2017-02-22 21:37:43 -0300

    first files

---
+++ src/broker.ts
@@ -0,0 +1,93 @@
+//import { Observable } from 'rxjs/Observable';
+//import 'rxjs/add/operator/catch';
+//import 'rxjs/add/operator/map';
+//import { Promise } from 'promise';
+
+/**
+ * 
+ */
+export class Broker
+{
+    /*-------------------------------------------------------------------
+	 *				 		     ATTRIBUTES
+	 *-------------------------------------------------------------------*/
+    /**
+     * 
+     */
+    private static services:Array<any> = new Array();
+
+    /*-------------------------------------------------------------------
+	 *				 		     BEHAVIORS
+	 *-------------------------------------------------------------------*/
+     /**
+      * 
+      */
+     public static of( serviceName:string ):ServiceProxy 
+     {
+         //is it already cached?
+         if ( !Broker.services[serviceName] )
+         {
+             Broker.services[serviceName] = new ServiceProxy( serviceName );
+         }
+         
+         return Broker.services[serviceName];
+     }
+}
+
+/**
+ * 
+ */
+class ServiceProxy
+{
+    /*-------------------------------------------------------------------
+     *                           ATTRIBUTES
+     *-------------------------------------------------------------------*/
+    /**
+     * 
+     */
+    private serviceName:string;
+
+    /**
+     * 
+     */
+    private service:any;
+
+    /*-------------------------------------------------------------------
+     *                           CONSTRUCTOR
+     *-------------------------------------------------------------------*/
+    /**
+     * 
+     */
+    public constructor( serviceName: string ) 
+    {
+        this.serviceName = serviceName;
+        this.service = window[serviceName];
+        
+        if ( !this.service ) throw new Error("The service.js must be imported before use the broker in the main html.");
+    }
+    
+    /*-------------------------------------------------------------------
+     *                           BEHAVIORS
+     *-------------------------------------------------------------------*/
+    /**
+     * 
+     */
+    public promise( methodName:string, ...args ):Promise<any> 
+    {
+        if ( !this.service[methodName] ) throw new Error("The method '"+methodName+"' not exists in the service '"+this.serviceName+"'");
+        
+        const promise = new Promise<any>( (resolve, reject) => {
+            
+            this.service[methodName](args, {
+                callback:( result ) => {
+                    resolve( result );
+                },
+                errorHandler:( message ) => {
+                    reject( new Error(message) );
+                }
+            });
+        });
+        
+        return promise;
+    }
+}
